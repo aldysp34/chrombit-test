e91813032c0f396a60ad664f7935f30f5de6e33c
Muhammad Aldi Surya Putra <aldi.surya@shopee.com>
2024-01-24 15:17:15 +0700

    fix(blog): change dummy data struct to be exported

--- usecase/blog_usecase.go
+++ usecase/blog_usecase.go
@@ -14,28 +14,28 @@ type BlogUsecase interface {
 	CreateBlog(context.Context, dto.BlogRequest) (*dto.BlogResponse, error)
 }
 
-type blogDummyData struct {
+type BlogDummyData struct {
 	ID    int
 	Title string
 	Body  string
 	Slug  string
 }
 
-var blogData []blogDummyData
+var blogData []BlogDummyData
 
 type blogUsecase struct{}
 
 func NewBlogUsecase() BlogUsecase {
-	blogData = append(blogData, blogDummyData{1, "Introduction to Go Programming", "Go is a powerful programming language designed for simplicity and efficiency.", "go-programming-intro"})
-	blogData = append(blogData, blogDummyData{2, "Building RESTful APIs with Node.js", "Learn how to create RESTful APIs using Node.js and Express.", "nodejs-restful-apis"})
-	blogData = append(blogData, blogDummyData{3, "Python for Data Science: A Comprehensive Guide", "Explore the world of data science with Python and its rich ecosystem of libraries.", "python-data-science-guide"})
-	blogData = append(blogData, blogDummyData{4, "Web Development with React.js", "Build modern and interactive web applications with the React.js library.", "reactjs-web-development"})
-	blogData = append(blogData, blogDummyData{5, "Getting Started with Docker Containers", "An introduction to Docker and containerization for scalable and portable applications.", "docker-getting-started"})
-	blogData = append(blogData, blogDummyData{6, "Machine Learning Fundamentals", "Understand the basics of machine learning and its applications in various domains.", "machine-learning-fundamentals"})
-	blogData = append(blogData, blogDummyData{7, "Java Programming Best Practices", "Follow best practices to write clean, efficient, and maintainable Java code.", "java-best-practices"})
-	blogData = append(blogData, blogDummyData{8, "Introduction to Cybersecurity", "Learn the fundamentals of cybersecurity to protect your digital assets and data.", "cybersecurity-intro"})
-	blogData = append(blogData, blogDummyData{9, "iOS App Development with Swift", "Create native iOS applications using the Swift programming language and Xcode.", "swift-ios-app-development"})
-	blogData = append(blogData, blogDummyData{10, "Database Design and Optimization Techniques", "Design and optimize databases for improved performance and reliability.", "database-design-optimization"})
+	blogData = append(blogData, BlogDummyData{1, "Introduction to Go Programming", "Go is a powerful programming language designed for simplicity and efficiency.", "go-programming-intro"})
+	blogData = append(blogData, BlogDummyData{2, "Building RESTful APIs with Node.js", "Learn how to create RESTful APIs using Node.js and Express.", "nodejs-restful-apis"})
+	blogData = append(blogData, BlogDummyData{3, "Python for Data Science: A Comprehensive Guide", "Explore the world of data science with Python and its rich ecosystem of libraries.", "python-data-science-guide"})
+	blogData = append(blogData, BlogDummyData{4, "Web Development with React.js", "Build modern and interactive web applications with the React.js library.", "reactjs-web-development"})
+	blogData = append(blogData, BlogDummyData{5, "Getting Started with Docker Containers", "An introduction to Docker and containerization for scalable and portable applications.", "docker-getting-started"})
+	blogData = append(blogData, BlogDummyData{6, "Machine Learning Fundamentals", "Understand the basics of machine learning and its applications in various domains.", "machine-learning-fundamentals"})
+	blogData = append(blogData, BlogDummyData{7, "Java Programming Best Practices", "Follow best practices to write clean, efficient, and maintainable Java code.", "java-best-practices"})
+	blogData = append(blogData, BlogDummyData{8, "Introduction to Cybersecurity", "Learn the fundamentals of cybersecurity to protect your digital assets and data.", "cybersecurity-intro"})
+	blogData = append(blogData, BlogDummyData{9, "iOS App Development with Swift", "Create native iOS applications using the Swift programming language and Xcode.", "swift-ios-app-development"})
+	blogData = append(blogData, BlogDummyData{10, "Database Design and Optimization Techniques", "Design and optimize databases for improved performance and reliability.", "database-design-optimization"})
 	return &blogUsecase{}
 }
 
@@ -90,7 +90,7 @@ func (bu *blogUsecase) EditBlog(ctx context.Context, req dto.BlogRequest) (*dto.
 }
 
 func (bu *blogUsecase) CreateBlog(ctx context.Context, req dto.BlogRequest) (*dto.BlogResponse, error) {
-	newData := blogDummyData{
+	newData := BlogDummyData{
 		ID:    req.ID,
 		Body:  req.Body,
 		Title: req.Title,
